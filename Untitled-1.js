7280789796a1a6f601d2b0092f32f5c6fc7b7c9a
PowderedAndrew <ignacioalvarenga@hotmail.com>
2024-06-24 07:56:23 -0300

    primeiro commit

---
+++ Untitled-1.js
@@ -0,0 +1,5 @@
+var fighter = ["Ryu", "Ken", "Guile", "E-Honda", "Chun-Li", "Dhalsim", "Zanguief", "Blanka"]
+
+console.log("Selecione seu lutador")
+console.log(fighter)
+console.log("Você selecionou " + fighter[4])
---
+++ index.js
@@ -0,0 +1,8 @@
+var pokemon = ["Charmander", "Charmeleon", "Charizard"]
+var colectionpokemon = [
+    ["Charmander   ", "Masculino   ", 30],
+    ["Charmeleon   ", "Feminino   ", 80],
+    ["Charizard    ", "Indefinido   ", 40]
+]
+console.log("Nome do pokemon: " + colectionpokemon[2][0], "Genero: " + colectionpokemon[2][1], "Level: " + colectionpokemon[1][2])
+console.log("Pokemon >> " + pokemon[2], "cadastrado com sucesso")
